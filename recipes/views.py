f2857f9334e110dcf363048baea66e40401cdef6
vitorsaantos <vitorsantoos96@gmail.com>
2024-07-15 13:46:11 -0300

    Recomeçando o curso de Django.
    Mexendo na parte de Urls e views.

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('Sobre')
