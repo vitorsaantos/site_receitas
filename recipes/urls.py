8f910d6ba9ae324be07690c53c3443deee2c4b28
vitorsaantos <vitorsantoos96@gmail.com>
2024-08-02 18:06:56 -0300

    Curso Update 02/08/2024

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,9 @@
 
-from django.contrib import admin
 from django.urls import path
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 
 ]
