1f36ed32063b7ef88d29a77c7b732b1fda24971e
vitorsaantos <vitorsantoos96@gmail.com>
2024-08-05 15:07:36 -0300

    Course Update

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategorAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategorAdmin)
