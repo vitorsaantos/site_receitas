6639fd6170d69c24b58a09b1028bd33b581ea819
vitorsaantos <vitorsantoos96@gmail.com>
2024-08-14 15:54:01 -0300

    Courser Update

--- recipes/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-class RecipeURlsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = '123456'
-        print('Olá mundo')
-        assert 1 == 1, 'Um é igual a Um'
-        ...
--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = get_list_or_404(Recipe.objects.filter(is_published=True).order_by('-id'))
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes' : recipes,
     })
