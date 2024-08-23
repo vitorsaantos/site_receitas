d398a9a5d773eb875cf353f33083c4e2ba4911e9
vitorsaantos <vitorsantoos96@gmail.com>
2024-08-23 11:24:08 -0300

    Course Update

--- recipes/views.py
+++ recipes/views.py
@@ -4,27 +4,16 @@ from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
 
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 from recipes.models import Recipe
+import os
 
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
 
-    try: 
-        current_page = int(request.GET.get('page', 1))
-    except ValueError: 
-        current_page = 1
-
-
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page,
-    )
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes' : page_obj,
@@ -34,8 +23,11 @@ def home(request):
 def category(request,category_id):
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes' : recipes,
+        'recipes' : page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name}  - Category |',
     })
 
@@ -61,8 +53,12 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)    
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_ulr_query': f'&q={search_term}',
     })
