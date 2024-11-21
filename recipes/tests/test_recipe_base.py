cdc810be1a07a5a8c269c069684aee84ce6c2ed8
vitorsaantos <vitorsantoos96@gmail.com>
2024-11-21 20:23:53 -0300

    Teste with the selenium

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -58,6 +58,18 @@ class RecipeMixin:
             preparation_steps_is_html=preparation_steps_is_html ,
             is_published=is_published,
         )
+    
+    def make_recipe_in_batch(self, qtd=10):
+        recipes = []
+        for i in range(qtd):
+            kwargs = {
+                'title': f'Recipe Title {i}',
+                'slug': f'r{i}', 
+                'author_data': {'username': f'u{i}'}
+                }
+            recipe = self.make_recipe(**kwargs)
+            recipes.append(recipe)
+        return recipes
 
 class RecipeTestBase(TestCase, RecipeMixin):
     def setUp(self) -> None:
--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,11 +1,44 @@
 from .base import RecipeBaseFunctionalTest
 import pytest
+from unittest.mock import patch
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    @patch('recipes.views.PER_PAGE', new=3)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.sleep()
         self.assertIn('No recipes found here 😢', body.text)
+
+
+    @patch('recipes.views.PER_PAGE', new=3)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê um campo de busca com um texto "Search for a recipe"
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+
+        # Clica neste input e digita o termo de busca
+        # para encrontrar a recieta com o título desejado
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        # O usuário vê oque estava procurando na página
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text
+        )
