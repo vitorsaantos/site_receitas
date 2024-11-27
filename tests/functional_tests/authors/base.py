3c184022a44b2e8721d1444f63f88fa42f8799e3
vitorsaantos <vitorsantoos96@gmail.com>
2024-11-27 11:23:16 -0300

    Test selenium
    Testing login , form and logout with selenium

--- tests/functional_tests/authors/base.py
+++ tests/functional_tests/authors/base.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 from utils.browser import make_chrome_browser
+from selenium.webdriver.common.by import By
 
 class AuthorsBaseTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
@@ -14,3 +15,9 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
     
     def sleep(self, qtd=10):
         time.sleep(qtd)
+
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
+    
---
+++ tests/functional_tests/authors/test_authors_login.py
@@ -0,0 +1,93 @@
+import pytest
+from .base import AuthorsBaseTest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from selenium.webdriver.common.by import By
+
+
+@pytest.mark.functional_test
+class AuthorsLoginTest(AuthorsBaseTest):
+    def test_user_valid_data_can_login_successfully(self):
+        string_password = 'pass'
+        user = User.objects.create_user(username='my_user', password=string_password)
+
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+        
+        # Usuário digita seu usário e senha
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+
+        # Usuário envia o formulário
+        form.submit()
+
+        # Usuário vê a mensagem de login com sucesso e seu nome
+        self.assertIn(
+            f'You are logged in with {user.username}.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(self.live_server_url + reverse('authors:login_create'))
+
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+
+    def test_form_login_is_invalid(self):
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # E tenta enviar valores vazios
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys(' ')
+        password.send_keys(' ')
+
+        # Usuário envia o formulário
+        form.submit()
+
+        # Vê uma mensagem de erro na tela
+        self.assertIn(
+            'Invalid username ou password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+
+    def test_form_login_invalid_credentials(self):
+        # Usuário abre a página de login
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # E tenta enviar valores com dados que não correspondem
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys('invalid_user')
+        password.send_keys('invalid_password')
+
+        # Usuário envia o formulário
+        form.submit()
+
+        # Vê uma mensagem de erro na tela
+        self.assertIn(
+            'Invalid credentials.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
+
+
+
+
+
