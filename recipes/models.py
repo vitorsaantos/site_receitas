f3a1f4ae8876bcda60bc3bd88ec8e09c2ef1d046
vitorsaantos <vitorsantoos96@gmail.com>
2025-01-09 07:09:20 -0300

    DebugToolbar
    DebugToolbar

--- recipes/models.py
+++ recipes/models.py
@@ -28,8 +28,7 @@ class Recipe(models.Model):
     category = models.ForeignKey(Category, 
     on_delete=models.SET_NULL, null=True, blank=True, default=None,)
 
-    author = models.ForeignKey(User, 
-    on_delete=models.SET_NULL, null=True)        
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)        
 
 
     def __str__(self):
