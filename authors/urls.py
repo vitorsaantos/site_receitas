4819943c92938293fb4ba08dfea3ffff652cc26c
vitorsaantos <vitorsantoos96@gmail.com>
2025-01-21 07:17:45 -0300

    Creting profile User
    Created profile for user, created template page and url

--- authors/urls.py
+++ authors/urls.py
@@ -18,6 +18,8 @@ urlpatterns = [
          views.DashboardRecipeDelete.as_view(), name='dashboard_recipe_delete'),
     path('dashboard/recipe/<int:id>/edit/', 
          views.DashboardRecipe.as_view(), name='dashboard_recipe_edit'),
+    path('profile/<int:id>/', 
+         views.ProfileView.as_view(), name='profile'),
     ]
              
 
--- recipes/views.py
+++ recipes/views.py
@@ -47,7 +47,7 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
 
-        qs = qs.select_related('author', 'category')
+        qs = qs.select_related('author', 'category', 'author__profile')
         qs = qs.prefetch_related('tags')
         return qs
 
