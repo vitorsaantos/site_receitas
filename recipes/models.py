fb95ccc92e56fa9b608e623bf2d66141ff618425
vitorsaantos <vitorsantoos96@gmail.com>
2025-01-27 06:54:48 -0300

    Creating signal and more
    Signal was created to delete and save images in the cover.
    
    and in the model it was made so that when saving an image it would maintain a standard size. Using pillow

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.utils.text import slugify
 from tag.models import Tag
 from collections import defaultdict
 from django.utils.translation import gettext_lazy as _
+import os
+from django.conf import settings
+from PIL import Image
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
@@ -41,12 +44,40 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
     
+    @staticmethod 
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        oroginal_width, original_heigth = image_pillow.size
+
+        if oroginal_width < new_width:
+            image_pillow.close()
+            return
+        
+        new_heigth = round((new_width * original_heigth) / oroginal_width)
+
+        new_image = image_pillow.resize((new_width, new_heigth), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except FileExistsError:
+                ...
+
+        return saved
     
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
